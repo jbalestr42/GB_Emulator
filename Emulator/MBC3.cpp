cf1409d127c009d7fd4a0975eae9b71d7f0652e9
Julien Balestrieri <julien.balestrieri@gmail.com>
2023-09-25 16:49:10 +0200

    Fix rombank and add comments

--- Emulator/MBC3.cpp
+++ Emulator/MBC3.cpp
@@ -50,42 +50,47 @@ void MBC3::write8(size_t addr, uint8_t v)
 	// ROM Bank Id
 	else if (addr >= 0x2000 && addr <= 0x3FFF)
 	{
-		// This 5-bit register selects the ROM bank id for the 4000–7FFF region
-		_romBankId = (_romBankId & 0x80) | (v & 0x7F);
+		// This 7-bit register selects the ROM bank id for the 4000–7FFF region
+		_romBankId = std::max(v & 0x7F, 0x01);
 
-		int count = _cartridge.getRomBankCount();
-		int nbBits = static_cast<uint8_t>(log2(count));
-		uint8_t mask = (1 << nbBits) - 1;
-		_romBankId &= mask;
+		//int count = _cartridge.getRomBankCount();
+		//int nbBits = static_cast<uint8_t>(log2(count));
+		//uint8_t mask = (1 << nbBits) - 1;
+		//_romBankId &= mask;
 	}
 
-	// RAM Bank Id
+	// RAM Bank Id or RTC Register Select
 	else if (addr >= 0x4000 && addr <= 0x5FFF)
 	{
-		// This 2-bit register can be used to select a RAM Bank (32 KiB ram carts only),
-		// or to specify the upper two bits (bits 5-6) of the ROM Bank number (1 MiB ROM or larger carts only).
+		// As for the MBC1s RAM Banking Mode, writing a value in range for 0x00-0x03 maps 
+		// the corresponding external RAM Bank (if any) into memory at A000-BFFF
 		if (v < 4)
 		{
 			_isRtcEnabled = false;
 			_ramBankId = (v & 0b00000011);
 			_ramBankId = _cartridge.hasRam() ? _ramBankId % _cartridge.getRamBankCount() : _ramBankId;
 		}
+
+		// When writing a value of 0x08-0x0C, this will map the corresponding RTC register into memory at A000-BFFF
 		else
 		{
-			// TODO
 			_isRtcEnabled = true;
 		}
 	}
 
+	// Latch Clock Data
 	else if (addr >= 0x6000 && addr <= 0x7FFF)
 	{
 	}
 	else if (addr >= 0xA000 && addr <= 0xBFFF && _isRamEnabled)
 	{
+		// Access RTC register
 		if (_isRtcEnabled)
 		{
 			// TODO
 		}
+
+		// Access RAM
 		else
 		{
 			_ram[addr - 0xA000 + (_ramBankId * Cartridge::RAM_BANK_SIZE)] = v;
